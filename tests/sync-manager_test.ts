74175a5830038c77ec8d34111996681f34f383a3
jeremiaholoh <davidjoseph3310@gmail.com>
2025-01-31 21:19:00 +0100

    test: Add sync manager tests

---
+++ tests/sync-manager_test.ts
@@ -0,0 +1,20 @@
+import { Clarinet, Tx, Chain, Account, types } from 'https://deno.land/x/clarinet@v1.0.0/index.ts';
+import { assertEquals } from 'https://deno.land/std@0.90.0/testing/asserts.ts';
+
+Clarinet.test({
+  name: "Ensure can update sync state",
+  async fn(chain: Chain, accounts: Map<string, Account>) {
+    const deployer = accounts.get("deployer")!;
+    
+    let block = chain.mineBlock([
+      Tx.contractCall("sync-manager", "update-sync-state",
+        [types.uint(1234)],
+        deployer.address
+      )
+    ]);
+    
+    assertEquals(block.receipts.length, 1);
+    assertEquals(block.height, 2);
+    assertEquals(block.receipts[0].result, '(ok true)');
+  },
+});
